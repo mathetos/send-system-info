4271e9ce810ea68784832e8e33c61ecf7cd73be1
johnregan3 <john.regan@x-team.com>
2014-05-05 14:32:14 -0500

    Documentation

--- includes/viewer.php
+++ includes/viewer.php
@@ -22,7 +22,7 @@ class Send_System_Info_Viewer {
 		$value = get_option( 'system_info_remote_url' );
 		$url   = home_url() . '/?systeminfo=' . $value;
 		?>
-		<p><?php _e( 'Users with this URL can view a plain-text version of your System Info.<br />This link can be handy in support forums, as access to this information can be removed after you recieve the help you need.<br />Generating a new URL will void access to all who have the existing URL.', 'send-system-info' ) ?></p>
+		<p><?php _e( 'Users with this URL can view a plain-text version of your System Info.<br />This link can be handy in support forums, as access to this information can be removed after you recieve the help you need.<br />Generating a new URL will safely void access to all who have the existing URL.', 'send-system-info' ) ?></p>
 		<p><input type="text" readonly="readonly" class="ssi-url ssi-url-text" onclick="this.focus();this.select()" value="<?php echo esc_url( $url ) ?>" title="<?php _e( 'To copy the System Info, click below then press Ctrl + C (PC) or Cmd + C (Mac).', 'send-system-info' ); ?>" />&nbsp;&nbsp;<a href="<?php echo esc_url( $url ) ?>" target="_blank" class="ssi-tiny ssi-url-text-link"><?php _e( 'test url', 'send-system-info' ) ?></a></p>
 		<p class="submit">
 			<input type="submit" onClick="return false;" class="button-secondary" name="generate-new-url" value="<?php _e( 'Generate New URL', 'send-system-info' ) ?>" />
